9ffab1a9da2028eea215245ca7e001643d36c91e
Sindre Bjerklund Lindberg <sindre.bjerklund.lindberg@gyldendal.no>
2026-03-04 13:45:23 +0100

    feat/Create Hero

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,15 +2,20 @@
 
 import Header from "~/components/ui/Header";
 import Footer from "~/components/ui/Footer";
+import Hero from "~/components/ui/home/Hero";
 
 export default function HomePage() {
   const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
-      <div className="h-1000"></div>
+
+      <main className="max-w-7xl mx-auto px-8 py-12">
+        <Hero />
+      </main>
+
       <Footer onScrollToTop={scrollToTop} />
-    </>
+    </div>
   );
 }
